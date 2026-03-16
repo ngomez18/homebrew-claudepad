f0402c18733508c69c689bb06a7db22f9bce80ea
Nicolás Gómez <n@gomezgg.com>
2026-03-16 09:24:57 -0500

    release v0.0.1

---
+++ Casks/claudepad.rb
@@ -0,0 +1,12 @@
+cask "claudepad" do
+  version "0.0.1"
+  sha256 "d23fb31f82b4dba94ce76dc55ea320357d8cdb9671a20bc768893f9a137b3bd1"
+
+  url "https://github.com/ngomez18/claudepad/releases/download/v#{version}/Claudepad.dmg"
+
+  name "Claudepad"
+  desc "Desktop app to enhance Claude Code"
+  homepage "https://github.com/ngomez18/claudepad"
+
+  app "claudepad.app"
+end
